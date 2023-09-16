3bc36b362e81a7d317943154cad61cd1bef27426
Joaquín Frías <joaquin124507@gmail.com>
2023-09-16 13:02:31 -0300

    Práctica aplicación TO DO LIST

---
+++ agregarTarea.php
@@ -0,0 +1,35 @@
+<?php
+try{
+$conexion = new PDO('mysql:host=localhost;dbname=aplicacion','root','');
+} catch(PDOException $e){
+    echo "Error de conexión".$e->getMessage();
+}
+
+if(isset($_POST['agregar_tarea'])){
+    
+    $tarea=($_POST['tarea']);
+    $sql='INSERT INTO tareas (tarea) VALUE(?)';
+    $sentencia=$conexion->prepare($sql);
+    $sentencia->execute([$tarea]);
+}
+
+if(isset($_GET['id'])){
+
+    $id=$_GET['id'];
+    $sql='DELETE FROM tareas WHERE id=?';
+    $sentencia=$conexion->prepare($sql);
+    $sentencia->execute([$id]);
+}
+
+if(isset($_POST['id'])){
+    $id=$_POST['id'];
+    $completado=(isset($_POST['completado']))?1:0;
+    $sql='UPDATE tareas SET completado=? WHERE id=?';
+    $sentencia=$conexion->prepare($sql);
+    $sentencia->execute([$completado,$id]);
+}
+
+$sql="SELECT * FROM tareas";
+$registros=$conexion->query($sql);
+
+?>
